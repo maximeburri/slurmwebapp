6db285a07cdb1a20f098c1950ff26d6acf499dbc
Maxime BURRI <maxime.burri@etu.hesge.ch>
2015-11-22 11:34:27 +0100

    Add loading icon in file browser

--- client/src/js/controllers/browser-ctrl.js
+++ client/src/js/controllers/browser-ctrl.js
@@ -8,13 +8,16 @@ angular.module('RDash')
 function BrowserCtrl($scope, User) {
     $scope.files = [];
     $scope.currentDir = ".";
+    $scope.loading = false;
     var scope = $scope;
     var that = this;
 
     $scope.updateFiles = function(dir){
+        $scope.loading = true;
         User.get("files", {dir:dir}, function(data, errorcode){
             $scope.currentDir = data.path;
             $scope.files = data.files;
+            $scope.loading = false;
             console.log("Operation effectued");
             console.log(JSON.stringify(data));
             console.log(errorcode);
