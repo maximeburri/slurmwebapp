27971e9756c3a610a800961422f320a12d38d9b4
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-02-03 17:32:52 +0100

    Change icon in job detail

--- client/src/js/directives/jobState.js
+++ client/src/js/directives/jobState.js
@@ -5,20 +5,25 @@
 
 angular
     .module('RDash')
-    .directive('swaJobState', ['Jobs', swaJobState]);
+    .directive('swaJobState', ['Jobs','$compile', swaJobState]);
 
-function swaJobState(Jobs) {
+function swaJobState(Jobs, $compile) {
     var directive = {
         restrict: 'AE',
         scope: {state: '@', description:'@', reason:'@'},
         template: '<i class="fa fa-{{icon.name}} {{icon.class}}" style="margin:5px;font-size: 18px!important;" tooltip="{{tooltip}}" tooltip-placement="top"> </i> ',
         link: function (scope, element, attrs) {
-            scope.icon = Jobs.jobStateCodes[scope.state].icon;
-            scope.tooltip = Jobs.jobStateCodes[scope.state].completeName ;
-            if(scope.description)
-                scope.tooltip += " : " + Jobs.jobStateCodes[scope.state].description;
-            /*if(scope.reason)
-                scope.tooltip += ", raison : " + scope.reason;*/
+            update = function(){
+                if(Jobs.jobStateCodes[scope.state]){
+                    scope.icon = Jobs.jobStateCodes[scope.state].icon;
+                    scope.tooltip = Jobs.jobStateCodes[scope.state].completeName ;
+                    if(scope.description)
+                        scope.tooltip += " : " + Jobs.jobStateCodes[scope.state].description;
+                    /*if(scope.reason)
+                        scope.tooltip += ", raison : " + scope.reason;*/
+                }
+            }
+            scope.$watch('state', update);
         }
     };
     return directive;
