7b1f0b282937dd8dd01e8a4e6ccff27fbef54bc6
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-05-17 11:07:10 +0200

    Parse time limit to get days, hours, ...

--- server/operations/partitions/ListPartitionsOperation.js
+++ server/operations/partitions/ListPartitionsOperation.js
@@ -39,7 +39,43 @@ function(result, exitcode, clientCallback){
                     splited.length >= 2 ? splited[1] : undefined;
             }
         }
+        if(partitionObject['MaxTime'] != undefined){
+            var MaxTime = {};
+            MaxTime.String = partitionObject['MaxTime'];
+            if(MaxTime.String != 'UNLIMITED'){
+                var ddhh_mm_ss = MaxTime.String.split(":");
+                var dd_hh = ddhh_mm_ss.length > 1 ?
+                                ddhh_mm_ss[0].split('-') :
+                                [];
 
+                MaxTime.Days = dd_hh.length > 1 ?
+                                parseInt(dd_hh[0]) :
+                                0;
+
+                MaxTime.Hours = dd_hh.length > 1 ?
+                                parseInt(dd_hh[1]) :
+                                parseInt(dd_hh[0]);
+
+                MaxTime.Minutes = ddhh_mm_ss.length > 2 ?
+                                parseInt(ddhh_mm_ss[1]) :
+                                0;
+
+                MaxTime.Seconds = ddhh_mm_ss.length > 3 ?
+                                parseInt(ddhh_mm_ss[2]) :
+                                0;
+
+                MaxTime.Timestamp =
+                                MaxTime.Seconds +
+                                MaxTime.Minutes * 60 +
+                                MaxTime.Hours * 60 * 60 +
+                                MaxTime.Days * 60 * 60 * 24;
+
+                MaxTime.Unlimited = false;
+            }else{
+                MaxTime.Unlimited = true;
+            }
+            partitionObject['MaxTime'] = MaxTime;
+        }
         partitionsListParsed[partitionObject['PartitionName']] = partitionObject;
     });
 
