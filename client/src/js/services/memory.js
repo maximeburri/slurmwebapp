95b5b9a03816e5495317cea410fc7df91f82cb8f
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-05-23 14:50:38 +0200

    Change algo

--- client/src/js/services/memory.js
+++ client/src/js/services/memory.js
@@ -1,26 +1,25 @@
 angular.module('RDash').service('Memory', [Memory]);
 
 function Memory() {
-    this.toBytes = function(number, stringUnit) {                
+    this.units = [
+        ["o", "B", "octets", "bytes"],
+        ["Ko", "KB"],
+        ["Mo", "MB"],
+        ["Go", "GB"],
+        ["To", "TB"],
+    ];
+    this.base = 1024;
+    var self = this;
+
+    this.toBytes = function(number, stringUnit) {
         factorUnit = 1;
-        switch (stringUnit) {
-            case "KB":
-            case "Ko":
-                factorUnit *= 1024;
-                break;
-            case "MB":
-            case "Mo":
-                factorUnit *= 1024^2;
-                break;
-            case "GB":
-            case "Go":
-                factorUnit *= 1024^3;
-                break;
-            case "TB":
-            case "To":
-                factorUnit *= 1024^4;
-                break;
+        for(i = 0;i<self.units.length;i++){
+            for(j = 0;j<self.units[i].length;j++){
+                if(stringUnit == self.units[i][j])
+                    return number * Math.pow(self.base, i);
+            }
         }
-        return number * factorUnit;
+
+        return number;
     };
 }
