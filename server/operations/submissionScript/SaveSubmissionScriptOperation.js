f684d2dd27155259dee08e799d7d00c27964a84d
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-06-06 13:46:14 +0200

    Add save file batch

--- server/operations/submissionScript/SaveSubmissionScriptOperation.js
+++ server/operations/submissionScript/SaveSubmissionScriptOperation.js
@@ -15,22 +15,48 @@ inherits(SaveSubmissionScriptOperation, ContentFileOperation);
 SaveSubmissionScriptOperation.prototype.makeOperation =
 function(client, operationInfo, clientCallback) {
     job = operationInfo.params.job;
-    rewriteScriptFilePath = operationInfo.params.rewriteScriptFilePath;
-    onlyVisualisation = operationInfo.params.onlyVisualisation;
+    readScriptFilePath = operationInfo.params.readScriptFilePath;
     saveScriptFilePath = operationInfo.params.saveScriptFilePath;
+    onlyVisualization = operationInfo.params.onlyVisualization;
+
+    self = this;
+
+    if(!readScriptFilePath && !saveScriptFilePath){
+        clientCallback(null, {type:"bad_parameters",
+            description:
+                "readScriptFilePath or saveScriptFilePath must be specified"});
+    }
 
     if(!job)
-        clientCallback(null, {type:"bad_parameters"});
+        clientCallback(null, {type:"bad_parameters",
+            description:
+                "'job' must not be empty"});
 
-    if(rewriteScriptFilePath){
-        this.readFile(client, clientCallback, rewriteScriptFilePath,
+    if(!saveScriptFilePath){
+        saveScriptFilePath = readScriptFilePath;
+    }
+
+    if(readScriptFilePath){
+        this.readFile(client, clientCallback, readScriptFilePath,
             function(fileContent){
             script = ScriptSubmission.save(job, fileContent);
-            clientCallback({script:script});
+
+            // If no visualization, save
+            if(!onlyVisualization)
+                self.writeFile(client, clientCallback, script,
+                        saveScriptFilePath);
+            else
+                clientCallback({script:script});
         });
     }else{
         script = ScriptSubmission.save(job);
-        clientCallback({script:script});
+
+        // If no visualization, save
+        if(!onlyVisualization)
+            self.writeFile(client, clientCallback, script,
+                    saveScriptFilePath);
+        else
+            clientCallback({script:script});
     }
 };
 
@@ -61,6 +87,40 @@ function(client, clientCallback, file, finishCallback){
             }
         }
     );
+}
+
+SaveSubmissionScriptOperation.prototype.writeFile =
+function(client, clientCallback, content, file){
+    // make a backup
+    this.makeBackupFile(client, function(exitCode){
+
+        // Then echo file
+        cmd = "echo '" + content + "' > "+file;
+        client.executeCommand(cmd,
+        function(result, exitcodeFinal, paramsCallback){
+            if(exitcodeFinal == 0)
+                clientCallback({"writted":true});
+            else
+                clientCallback(null, {exitcode : exitcodeFinal});
+        }, {},
+        function(result, data, paramsCallback){
+
+        });
+    },
+    file);
+}
+
+SaveSubmissionScriptOperation.prototype.makeBackupFile =
+function(client, finishCallback, file ){
+    cmd = "mv " + file + " " + file + ".backup";
+
+    client.executeCommand(cmd,
+    function(result, exitcodeFinal){
+        finishCallback(exitcodeFinal);
+    }, {},
+    function(result, data){
+        // Stderr
+    });
 }
 
 // On quit nothing to do, because no tail
