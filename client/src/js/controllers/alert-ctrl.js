165cfb7b0d97a5e6bac3918f1fe331dab46525c2
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-06-09 17:16:02 +0200

    Add job stat

--- client/src/js/controllers/alert-ctrl.js
+++ client/src/js/controllers/alert-ctrl.js
@@ -4,15 +4,21 @@
 
 angular
     .module('RDash')
-    .controller('AlertsCtrl', ['$rootScope', AlertsCtrl]);
+    .controller('AlertsCtrl', ['$rootScope','User', AlertsCtrl]);
 
-function AlertsCtrl($rootScope) {
+function AlertsCtrl($rootScope, User) {
+    if(!$rootScope.cluster){
+        User.get('cluster').then(
+            function(data){
+                $rootScope.cluster = data.cluster;
+                console.log(data);
+            },
 
-    $rootScope.addAlert = function() {
-        $rootScope.alerts.push({
-            msg: 'Another alert!'
-        });
-    };
+            function(data){
+                console.error(data);
+            }
+        )
+    }
 
     $rootScope.closeAlert = function(index) {
         $rootScope.alerts.splice(index, 1);
