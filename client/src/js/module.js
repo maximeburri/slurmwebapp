57a520159467f5f3aaad9bb01fbe0ddef643b5c2
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-06-10 12:03:24 +0200

    Add module for pie chart

--- client/src/js/module.js
+++ client/src/js/module.js
@@ -1,1 +1,2 @@
-angular.module('RDash', ['ngCookies', 'ui.bootstrap', 'ui.router', 'timer', 'nya.bootstrap.select']);
+angular.module('RDash', ['ngCookies', 'ui.bootstrap', 'ui.router',
+    'timer', 'nya.bootstrap.select', 'n3-pie-chart']);
