155f6d881a113e46df7b1861a8bb76ee78b6bda9
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-06-10 16:20:25 +0200

    Ste always update when go to dashbord

--- client/src/js/controllers/alert-ctrl.js
+++ client/src/js/controllers/alert-ctrl.js
@@ -4,79 +4,77 @@
 
 angular
     .module('RDash')
-    .controller('AlertsCtrl', ['$rootScope','User', AlertsCtrl]);
+    .controller('AlertsCtrl', ['$rootScope', 'User', AlertsCtrl]);
 
 function AlertsCtrl($rootScope, User) {
-    if(!$rootScope.cluster){
-        $rootScope.nodes = {
-            data : [{
-                label:"utilisés",
-                value:0,
-                color: "#ae2323",
-                suffix:'%'
-            }],
-            options : {
-                thickness: 10,
-                mode: "gauge",
-                total: 100}
-            };
-        $rootScope.cpus = {
-            data : [{
-                label: "utilisés",
-                value: 0,
-                color: "#d3a938",
-                suffix: '%'
-            }],
-            options : {
-                thickness: 10,
-                mode: "gauge",
-                total: 100}
-            };
+    $rootScope.cluster = null;
+    $rootScope.nodes = {
+        data : [{
+            label:"utilisés",
+            value:0,
+            color: "#ae2323",
+            suffix:'%'
+        }],
+        options : {
+            thickness: 10,
+            mode: "gauge",
+            total: 100}
+        };
+    $rootScope.cpus = {
+        data : [{
+            label: "utilisés",
+            value: 0,
+            color: "#d3a938",
+            suffix: '%'
+        }],
+        options : {
+            thickness: 10,
+            mode: "gauge",
+            total: 100}
+        };
 
-            $rootScope.jobs = {
-                data : [{
-                    label: "",
-                    value: 0,
-                    color: "#2361ae"
-                },{
-                    label: "",
-                    value: 0,
-                    color: "#23ae89"
-                }],
-                options : {
-                    thickness: 10
-                    }
-                };
-
-        User.get('cluster').then(
-            function(data){
-                $rootScope.cluster = data.cluster;
-
-                var allocatedNode =
-                    Math.round((data.cluster.statistics.nodes.allocated /
-                        data.cluster.statistics.nodes.total) * 1000)/10;
-
-                var allocatedCPUs =
-                    Math.round((data.cluster.statistics.cpus.allocated /
-                        data.cluster.statistics.cpus.total) * 1000)/10;
-
-                $rootScope.nodes.data[0].value = allocatedNode;
-
-                $rootScope.cpus.data[0].value = allocatedCPUs;
-
-                $rootScope.jobs.data[0].value = data.cluster.statistics.jobs.pending;
-                $rootScope.jobs.data[0].label = $rootScope.jobs.data[0].value + " en attente";
-                $rootScope.jobs.data[1].value = data.cluster.statistics.jobs.running;
-                $rootScope.jobs.data[1].label = $rootScope.jobs.data[1].value + " en cours";
-
-                console.log(data);
-            },
-
-            function(data){
-                console.error(data);
+    $rootScope.jobs = {
+        data : [{
+            label: "",
+            value: 0,
+            color: "#2361ae"
+        },{
+            label: "",
+            value: 0,
+            color: "#23ae89"
+        }],
+        options : {
+            thickness: 10
             }
-        )
-    }
+        };
+
+    User.get('cluster').then(
+        function(data){
+            $rootScope.cluster = data.cluster;
+
+            var allocatedNode =
+                Math.round((data.cluster.statistics.nodes.allocated /
+                    data.cluster.statistics.nodes.total) * 1000)/10;
+
+            var allocatedCPUs =
+                Math.round((data.cluster.statistics.cpus.allocated /
+                    data.cluster.statistics.cpus.total) * 1000)/10;
+
+            $rootScope.nodes.data[0].value = allocatedNode;
+
+            $rootScope.cpus.data[0].value = allocatedCPUs;
+
+            $rootScope.jobs.data[0].value = data.cluster.statistics.jobs.pending;
+            $rootScope.jobs.data[0].label = $rootScope.jobs.data[0].value + " en attente";
+            $rootScope.jobs.data[1].value = data.cluster.statistics.jobs.running;
+            $rootScope.jobs.data[1].label = $rootScope.jobs.data[1].value + " en cours";
+            console.log(data);
+        },
+
+        function(data){
+            console.error(data);
+        }
+    )
 
     $rootScope.closeAlert = function(index) {
         $rootScope.alerts.splice(index, 1);
