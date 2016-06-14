1d9399adbaadb26107e25df4d897743a2f11beed
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-06-14 15:50:58 +0200

    Add new file operation

--- server/objects/FileObject.js
+++ server/objects/FileObject.js
@@ -5,13 +5,15 @@ var ContentFileOperation = require('../operations/file/ContentFileOperation.js')
 var UploadFileOperation = require('../operations/file/UploadFileOperation.js');
 var RemoveFileOperation = require('../operations/file/RemoveFileOperation.js');
 var MoveFileOperation = require('../operations/file/MoveFileOperation.js');
+var NewFileOperation = require('../operations/file/NewFileOperation.js');
 
 function FileObject() {
     ObjectOperation.call(this, {
         "get" : new ContentFileOperation(),
         "upload" : new UploadFileOperation(),
         "remove" : new RemoveFileOperation(),
-        "move" : new MoveFileOperation()
+        "move" : new MoveFileOperation(),
+        "new" : new NewFileOperation(),
     });
 }
 
---
+++ server/operations/file/NewFileOperation.js
@@ -0,0 +1,50 @@
+var Operation = require('../Operation.js');
+var shellescape = require('shell-escape');
+var inherits = require('util').inherits;
+
+function NewFileOperation() {
+    Operation.call(this);
+}
+inherits(NewFileOperation, Operation);
+
+// Overwrite
+NewFileOperation.prototype.makeOperation =
+function(client, operationInfo, clientCallback) {
+    newFilepath = operationInfo.params.newFilepath;
+    type = operationInfo.params.type;
+
+    if(!newFilepath && !type)
+        clientCallback(null, {type:"REQUIRED_PARAMETERS", operationInfo:operationInfo});
+
+    var cmd = "";
+
+    // Type folder, mkdir
+    if(type == "folder")
+        cmd = shellescape(["mkdir", newFilepath]);
+    // Type file, touch
+    else if (type == "file")
+        cmd = shellescape(["touch", newFilepath]);
+    // Else, bad type
+    else
+        clientCallback(null, {type:"BAD_TYPE", operationInfo:operationInfo});
+
+    client.executeCommand(cmd, this.finish, clientCallback);
+};
+
+// Parse liste files (ls -aF)
+NewFileOperation.prototype.finish =
+function(result, exitcode, clientCallback){
+    // An error has occured
+    if (exitcode != 0){
+        clientCallback(null, {code:exitcode, type:"NEW_FILE_ERROR"});
+        return;
+    }else{
+        clientCallback({new_success:true}, false);
+        return
+    }
+}
+
+
+
+// export the class
+module.exports = NewFileOperation;
