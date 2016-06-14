f9407d1b654876baa3f7b2c137a8de8d23f7593e
Maxime BURRI <maxime.burri@etu.hesge.ch>
2016-06-14 13:35:40 +0200

    Add upload operation

--- server/objects/FileObject.js
+++ server/objects/FileObject.js
@@ -2,10 +2,12 @@ var ObjectOperation = require('./ObjectOperation.js');
 var inherits = require('util').inherits;
 
 var ContentFileOperation = require('../operations/file/ContentFileOperation.js');
+var UploadFileOperation = require('../operations/file/UploadFileOperation.js');
 
 function FileObject() {
     ObjectOperation.call(this, {
         "get" : new ContentFileOperation(),
+        "upload" : new UploadFileOperation(),
     });
 }
 
---
+++ server/operations/file/UploadFileOperation.js
@@ -0,0 +1,37 @@
+var Operation = require('../Operation.js');
+var shellescape = require('shell-escape');
+var inherits = require('util').inherits;
+
+function UploadFileOperation() {
+    Operation.call(this);
+}
+inherits(UploadFileOperation, Operation);
+
+// Overwrite
+UploadFileOperation.prototype.makeOperation =
+function(client, operationInfo, clientCallback) {
+    filepath = operationInfo.params.filepath;
+    data = operationInfo.params.data;
+    //var cmd = shellescape(["printf" , "%s", data]) + " > " + filename;
+    var cmd = shellescape(["echo", data.toString('base64')]) +
+             ' | base64 --decode > ' + shellescape([filepath]);
+    client.executeCommand(cmd, this.finish, clientCallback);
+};
+
+// Parse liste files (ls -aF)
+UploadFileOperation.prototype.finish =
+function(result, exitcode, clientCallback){
+    // An error has occured
+    if (exitcode != 0){
+        clientCallback(null, {code:exitcode, type:"UPLOAD_ERROR"});
+        return;
+    }else{
+        clientCallback({upload_success:true}, false);
+        return
+    }
+}
+
+
+
+// export the class
+module.exports = UploadFileOperation;
