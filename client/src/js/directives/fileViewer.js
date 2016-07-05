5c6de93cd12444b6950b7b92b602633f4c3ffc73
Maxime Burri <maxime.burri@gmail.com>
2016-07-05 11:12:08 +0200

    Fix bug unicode characters

--- client/src/js/directives/fileViewer.js
+++ client/src/js/directives/fileViewer.js
@@ -39,12 +39,10 @@ function swaFileViewer(User, Files, $modal, $compile) {
             }
 
             function str2ab(str) {
-                  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-                  var bufView = new Uint16Array(buf);
-                  for (var i=0, strLen=str.length; i<strLen; i++) {
-                      bufView[i] = str.charCodeAt(i);
-                  }
-                  return buf;
+                //https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+                return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+                      return String.fromCharCode('0x' + p1);
+                  }));
             }
 
             scope.save = function(){
